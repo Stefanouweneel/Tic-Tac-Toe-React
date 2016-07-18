07c87f1237980be44e6736875f8d7aab08afbdd8
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-07-18 16:40:00 +0200

    Make tiles render in columns and center content

--- src/components/Game.js
+++ src/components/Game.js
@@ -13,23 +13,28 @@ class Game extends React.Component {
         '', '', ''
       ]
     }
+
+    this.baseStyle = {
+      display: 'flex',
+      justifyContent: 'center',
+    }
   }
 
   render() {
     return (
       <div id='game'>
         <Header />
-          <div>
+          <div style={this.baseStyle}>
           <Tiles position={1} />
           <Tiles position={2} />
           <Tiles position={3} />
         </div>
-        <div>
+        <div style={this.baseStyle}>
           <Tiles position={4} />
           <Tiles position={5} />
           <Tiles position={6} />
         </div>
-        <div>
+        <div style={this.baseStyle}>
           <Tiles position={7} />
           <Tiles position={8} />
           <Tiles position={9} />
