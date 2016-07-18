e19cec4484abeb06bf913de2fd30c6bb8152edc0
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-07-18 13:15:17 +0200

    Add state for tiles and render them

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Game from './components/Game';
 
 class App extends React.Component {
-    render() {
-        return (
-            <h1>Hello World!</h1>
-        );
-    }
+  render() {
+    return (
+      <div>
+      <h1>Hello World!</h1>
+        <Game />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import Canvas from './components/Canvas';
-import Header from './components/Header';
 
 class Game extends React.Component {
-    render() {
-        return (
-            <h1>Hello World!</h1>
-        );
+  constructor() {
+    super();
+
+    this.state = {
+      tiles: [
+        '', '', '',
+        '', '', '',
+        '', '', ''
+      ],
+      turn: 'o'
     }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.tiles}
+      </div>
+    );
+  }
 }
 
 export default Game;
